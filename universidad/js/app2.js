18672aa30341006b2fb962c0350b9667e7123565
alfredo <alfredolopeznunes@gmail.com>
2020-05-21 22:45:12 -0400

    fin clases 54

---
+++ universidad/js/app2.js
@@ -0,0 +1,29 @@
+// Event Listener Click
+
+// Buscador
+// opcion 1
+document.querySelector('#submit-buscador').addEventListener('click', function(e){
+    e.preventDefault();
+    alert('Click al buscador');
+});
+
+// opcion 2
+document.querySelector('#submit-buscador').addEventListener('click', ejecutaBoton);
+
+function ejecutaBoton(ev) {
+    ev.preventDefault();
+    console.log('Desde la función ejecutar Boton');
+
+    let evElemento;
+    evElemento = ev;
+    evElemento = ev.target;
+    evElemento = ev.target.id;
+    evElemento = ev.target.className;
+    // evElemento = ev.target.innerText;
+
+    console.log(evElemento);
+};
+
+document.querySelector('#encabezado-reemplazado-js').addEventListener('click', function(e){
+    e.target.innerText = "Nuevo encabezado con Event Listener";
+});
